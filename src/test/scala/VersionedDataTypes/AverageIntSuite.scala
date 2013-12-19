db959617f3827c943c901d254e19bceec647c948
Nikita Putintsev <Putintsevnikita@gmail.com>
2013-12-19 18:25:19 +0700

    Concurrency test

---
+++ src/test/scala/VersionedDataTypes/AverageIntSuite.scala
@@ -0,0 +1,66 @@
+package VersionedDataTypes
+
+import revisions.Revision
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import scala.concurrent._
+import scala.concurrent.duration._
+
+@RunWith(classOf[JUnitRunner])
+class AverageIntSuite extends FunSuite{
+  
+  val testVer: AverageInt = new AverageInt
+  
+  test("Simple put") {
+    val testVal = 10
+    testVer.setItem(testVal)
+    assert(testVer.getItem == testVal)
+  }
+  
+  test("Got average after join") {
+    val testVal1 = 10
+    val testVal2 = 20
+    testVer.setItem(testVal1)
+    def task():Unit = {
+      testVer.setItem(testVal2)
+      assert(testVer.getItem == testVal2)
+      }
+    val r1: Revision = Revision.mainRevision.fork(task)
+    assert(testVer.getItem == testVal1)
+    
+    Revision.mainRevision.hardJoin(r1)
+    assert(testVer.getItem == (testVal2 + testVal1)/2)
+  }
+  
+  test("Netsed revision") {
+    val testVal1 = 100
+    val testVal2 = 200
+    val testVal3 = 300
+    testVer.setItem(testVal1)
+    
+    def task1():Unit = {
+      testVer.setItem(testVal2)
+      //println ("task1")
+      assert(testVer.getItem == testVal2)
+      }
+    
+     def task2():Unit = {
+      assert(testVer.getItem == testVal2)
+      //println ("task2")
+      testVer.setItem(testVal3)
+      assert(testVer.getItem == testVal3)
+      }
+    
+    val r1: Revision = Revision.mainRevision.fork(task1)
+    val r2: Revision = r1.fork(task2)
+    
+    assert(testVer.getItem == testVal1)
+    
+    //println ("wtf")
+    r1.tailJoin(r2)
+    Revision.mainRevision.hardJoin(r1)	
+    assert(testVer.getItem == ((testVal3 + testVal2)/2 + testVal1)/2)
+  }
+  
+}
---
+++ src/test/scala/revisions/ConcurrenceTests.scala
@@ -0,0 +1,46 @@
+package revisions
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import org.scalatest.prop.Checkers
+import org.scalacheck._
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Prop
+import org.scalacheck.Prop._
+
+@RunWith(classOf[JUnitRunner])
+class ConcurrenceTests extends FunSuite{
+  def smallDelay = Gen.choose(0, 50)
+  
+ val determinanceProp = Prop.forAll(smallDelay, smallDelay, smallDelay)((d1, d2, d3) => nestedMerge(d1, d2, d3) == d3)
+  
+ def nestedMerge(v1: Int, v2: Int, v3: Int) : Int = 
+  {
+    val testVer = new VersionedItem[Int]
+    testVer.setItem(v1)
+    
+    def task1():Unit = {
+      testVer.setItem(v2)
+      Thread.sleep(v1)
+      }
+    
+     def task2():Unit = {
+      testVer.setItem(v3)
+      Thread.sleep(v2)
+      }
+    
+    val r1: Revision = Revision.mainRevision.fork(task1)
+    val r2: Revision = r1.fork(task2)
+    Thread.sleep(v3)
+    r1.tailJoin(r2)
+    Revision.mainRevision.hardJoin(r1)	
+    testVer.getItem(Revision.mainRevision)
+  }
+  
+  test("Concuurent merge is determenistic") {
+     determinanceProp.check
+  }
+}
