2be44753621283f1baefa9e60d268b4d614ec58f
Alexander <alexander.zyryanov44@gmail.com>
2013-12-25 12:47:03 +0700

    Complete versioned stack data structure

--- src/main/scala/VersionedColls/VersionedStack.scala
+++ src/main/scala/VersionedColls/VersionedStack.scala
@@ -1,8 +1,9 @@
 package VersionedColls
 import revisions.VersionedObj
 import revisions.SpecialMerge
-import scala.collection.immutable.Stack
+import scala.collection.mutable.Stack
 import scala.collection.mutable.Queue
+import scala.xml.Null
 
 trait StackDAO[T] {
   this: VersionedStack[T] =>
@@ -10,38 +11,67 @@ trait StackDAO[T] {
   def push(item: T) : Unit = {
     val stack = this.getItem
     
-    this.setItem(stack.push(item))
+    stack.push(item)
   }
   
   def pop() : T = {
-    val tuple = this.getItem.pop2
+    val stack = this.getItem
     
-    tuple match {
-      case (item, stack) => 
-        this.setItem(stack)
-        item
-    }
+    stack.pop   
   }
 }
 
 trait StackMerge[T] extends SpecialMerge[Stack[T]] {
+  
+   /**
+   * Merge two stacks with rules:
+   * if joiner stack item equals joiny stack item, add this item to result stack
+   * if joiner stack item not equals joiny stack item, but root stack item equals one of them, add root item to result stack
+   * in other cases just add joiner stack item to result stack 
+   */
   override def mergeFunction(joiner: Stack[T], joiny: Stack[T], root: Stack[T]) : Stack[T] = {
-    val zipSeq = joiner zip joiny
-    var resultList = List[T]()
+    val resultStack = new Stack[T]
+    val joinyIterator = joiny.reverse.iterator
+    val rootIterator = root.reverse.iterator
+    val conflictQueue = new Queue[T]
     
-    /*for(itemPair <- zipSeq) {
-      itemPair match {
-        case (joinerItem, joinyItem) =>
-          if(joinerItem == joinyItem) {
-            resultList ::= joinerItem
-          } else {
-            resultList ::= joinerItem
-            resultList ::= joinyItem
-          }
+    def nextOption(iterator: Iterator[T]): Option[T]  = {
+      if(iterator.hasNext) {
+        Option(iterator.next)
+      } else {
+        None
       }
-    }*/
-    root
-    //new Stack[T](resultList.to)
+    }
+    
+  
+    for(joinerItem <- joiner.reverse) {
+    	val joinyOpt = nextOption(joinyIterator)
+    	val rootOpt = nextOption(rootIterator)
+    	
+    	joinyOpt match {
+    	  case Some(joinyItem) =>
+    	    if(joinerItem == joinyItem) {
+    	      resultStack.push(joinerItem)
+    	    } else {
+    	      rootOpt match {
+    	        case Some(rootItem) =>
+    	          if(rootItem == joinyItem) {
+    	            resultStack.push(rootItem)
+    	          } else {
+    	            resultStack.push(joinerItem)
+    	          }
+    	          
+    	        case None =>
+    	          resultStack.push(joinerItem)
+    	      }
+    	    }
+    	    
+    	  case None =>
+    	    resultStack.push(joinerItem)
+    	}
+    }
+   
+    resultStack
   }
     
 }
